62bca767aba5227d9ac077191869fb492eae9e12
Marianne <m.salamanca21@live.ca>
2022-02-05 17:14:18 -0500

    add selling class

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from .models import Nft, Selling
 # Register your models here.
 from .models import Nft
 admin.site.register(Nft)
+admin.site.register(Selling)
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,11 @@
+from xmlrpc.client import boolean
 from django.db import models
 from django.urls import reverse
+
+SELL = (
+    ('S', 'Selling'),
+    ('S', 'Sold')
+)
 
 # Create your models here.
 class Nft(models.Model):
@@ -13,3 +19,16 @@ class Nft(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs = {'nft_id': self.id})
+
+class Selling(models.Model):
+    date = models.DateField('Selling Date')
+    sell = models.CharField(
+        max_length=1,
+        choices=SELL,
+        default=SELL[0][0]
+    )
+
+    nft = models.ForeignKey(Nft, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_sell_display()} on {self.date}"
